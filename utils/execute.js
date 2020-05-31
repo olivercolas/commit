c5c9bc938be7586af134566bb8d463641331b06e
olivercolas <olivercolas.oc@gmail.com>
2020-05-31 22:01:35 +0100

    feat(functions): Added functions for git commands

--- utils/execute.js
+++ utils/execute.js
@@ -1,23 +1,29 @@
-const { execCommand } = require('./helpers');
+const { execCommand, execSilentCommand } = require('./helpers');
+
+const getCurrentBranch = async () => {
+  const gitCommand = 'git rev-parse --abbrev-ref HEAD';
+
+  return await execSilentCommand(gitCommand, `Couldn't get branch name`);
+};
+
+const commit = (files, type, scope, message) => {
+  let gitCommand = `git add ${files.join(' ')} && git commit -m "`;
+  if (type) gitCommand += type;
+  if (scope) gitCommand += `(${scope}): `;
+  if (message) gitCommand += `${message}"`;
+
+  execCommand(gitCommand, `Couldn't complete commit command`);
+};
+
+const pushToOrigin = async () => {
+  const branchName = await getCurrentBranch();
+  const gitCommand = `git push origin ${branchName}`;
+
+  execCommand(gitCommand, `Couldn't complete push command`);
+};
 
 module.exports = {
-  commit: (files, type, scope, message) => {
-    let gitCommand = `git add ${files.join(' ')} && git commit -m "`;
-    if (type) gitCommand += type;
-    if (scope) gitCommand += `(${scope}): `;
-    if (message) gitCommand += `${message}"`;
-
-    execCommand(gitCommand, `Couldn't complete commit command`)
-  },
-  push: () => {
-    const branchName = this.getCurrentBranch;
-    const gitCommand = `git push origin ${branchName}`;
-
-    execCommand(gitCommand, `Couldn't complete push command`)
-  },
-  getCurrentBranch: () => {
-    const gitCommand = 'git rev-parse --abbrev-ref HEAD';
-
-    execCommand(gitCommand, `Couldn't get branch name`)
-  }
+  commit,
+  getCurrentBranch,
+  pushToOrigin,
 };
