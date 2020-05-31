713ab109055fa2d3b2b66ff238ba5fe35c0ec125
olivercolas <olivercolas.oc@gmail.com>
2020-05-31 21:32:29 +0100

    feat: functions for general usage

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,26 @@
+const shell = require('shelljs');
+const exec = require('child_process').exec;
 const { log } = console;
 
+const logErrorAndExitProcess = (error) => {
+  log(error);
+  process.exit(1);
+};
 module.exports = {
-  logErrorAndExitProcess: (error) => {
-    log(error);
-    process.exit(1);
+  logErrorAndExitProcess,
+  execCommand: (command, errorMessage) => {
+    const commitExec = shell.exec(command);
+    if (errorMessage && commitExec.code !== 0) {
+      logErrorAndExitProcess(errorMessage);
+    }
+
+    return commitExec;
   },
+  execSilentCommand: (command, errorMessage) =>
+    new Promise((resolve, reject) => {
+      exec(command, (err, stdout) => {
+        if (err) logErrorAndExitProcess(errorMessage);
+        resolve(stdout);
+      });
+    }),
 };
