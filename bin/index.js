174554778fd46487db075e53f1d85e35fc450c64
olivercolas <olivercolas.oc@gmail.com>
2020-05-31 22:02:52 +0100

    feat(init): Updated run function

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,71 @@
 #!/usr/bin/env node
-const { log } = console;
+const arg = require('arg');
 const { stagedFiles } = require('../utils/stagedFiles');
-const { selectStagedFiles, getCommitType, getCommitScope } = require('../utils/inquirer');
+const {
+  selectStagedFiles,
+  confirmCommitType,
+  getCommitType,
+  confirmCommitScope,
+  getCommitScope,
+  getCommitMessage,
+  getPushOption,
+} = require('../utils/inquirer');
+const { commit, pushToOrigin } = require('../utils/execute');
+const { log } = console;
 
-const CURRENT_DIR_PATH = process.cwd();
+const args = arg({
+  '-t': Boolean,
+  '-s': Boolean,
+  '-p': Boolean,
+  '--add-type': '-t',
+  '--add-scope': '-s',
+  '--add-push': '-p',
+  '--push': '-p',
+  '--no-type': Boolean,
+  '--no-scope': Boolean,
+  '--no-push': Boolean,
+});
 
 const run = async () => {
   // ! must add case for when git respository doesn't exist
-  const filesAvailableForCommit = await stagedFiles();
+  let scope, type, message;
+  (confirmType = true), (confirmScope = true), (confirmPush = true);
 
-  const { filesToCommit } = await selectStagedFiles(filesAvailableForCommit);
-  log(filesToCommit);
-  const type = await getCommitType();
-  log(type)
-  const scope = await getCommitScope();
-  log(scope)
+  const filesAvailableForCommit = await stagedFiles();
+  const filesToCommit = await selectStagedFiles(filesAvailableForCommit);
+
+  if (!args['--no-type']) {
+    if (!args['-t']) {
+      confirmType = await confirmCommitType();
+    }
+
+    if (confirmType) {
+      type = await getCommitType();
+
+      if (!args['--no-scope']) {
+        if (!args['-s']) {
+          confirmScope = await confirmCommitScope();
+        }
+
+        if (type && confirmScope) {
+          scope = await getCommitScope();
+        }
+      }
+    }
+  }
+
+  message = await getCommitMessage();
+
+  commit(filesToCommit, type, scope, message);
+
+  if (!args['--no-push']) {
+    if (!args['-p']) {
+      confirmPush = await getPushOption();
+    }
+    if (confirmPush) {
+      await pushToOrigin();
+    }
+  }
 };
 
 run();
